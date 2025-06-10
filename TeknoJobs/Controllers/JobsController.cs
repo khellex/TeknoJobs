09691c8b91f6271add54d6caa94f97453baeaf47
Karthik Pai <kpai1996@gmail.com>
2025-06-11 01:36:13 +0530

    Setting up clean architecture
    - Added Domain, Application, Infrastructure, Persistence class libraries
    - Added organized folders
    - Discarded unnecessary controllers and methods
    - Introduced new API controllers, for jobs, locations and departments and auth

---
+++ TeknoJobs/Controllers/JobsController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeknoJobs.Controllers
+{
+    public class JobsController : ControllerBase
+    {
+    }
+}
---
+++ TeknoJobs/Controllers/LocationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeknoJobs.Controllers
+{
+    public class LocationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
